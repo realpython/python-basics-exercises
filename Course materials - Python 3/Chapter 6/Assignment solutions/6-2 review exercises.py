d61b9b9cbfae468b60b482c858f0bf4c9a49a865
fheisler <fheisler@gmail.com>
2014-07-19 22:15:40 -0400

    Var name updates

--- Course materials - Python 3/Chapter 6/Assignment solutions/6-2 review exercises.py
+++ Course materials - Python 3/Chapter 6/Assignment solutions/6-2 review exercises.py
@@ -3,8 +3,8 @@
 
 def cube(num):
     ''' Returns the cube of the input number '''
-    cubeNum = num * num * num
-    return cubeNum
+    cube_num = num * num * num
+    return cube_num
 
 print("0 cubed is", cube(0))
 print("2 cubed is", cube(2))
@@ -14,5 +14,5 @@ def multiply(num1, num2):
     ''' Returns the result of multiplying two input numbers '''
     return num1 * num2
 
-multResult = multiply(2, 5)
-print("2 times 5 is", multResult)
+mult_result = multiply(2, 5)
+print("2 times 5 is", mult_result)
