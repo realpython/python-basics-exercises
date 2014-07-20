eab834ac5ec0358c6739e62b1ddcb2a64f495671
fheisler <fheisler@gmail.com>
2014-07-19 21:09:52 -0400

    Var name updates

--- Course materials/Chapter 14/Assignment solutions/14-3 review exercises.py
+++ Course materials/Chapter 14/Assignment solutions/14-3 review exercises.py
@@ -4,29 +4,29 @@ import mechanize
 # from bs4 import BeautifulSoup
 
 # Start a mechanize browser and open the login page
-myBrowser = mechanize.Browser()
-myBrowser.open("http://RealPython.com/practice/login.php")
+my_browser = mechanize.Browser()
+my_browser.open("http://RealPython.com/practice/login.php")
 
 # Select the login form and input a username & password
-myBrowser.select_form("login")
-myBrowser["user"] = "zeus"
-myBrowser["pwd"] = "ThunderDude"
-myResponse = myBrowser.submit()  # submit form
+my_browser.select_form("login")
+my_browser["user"] = "zeus"
+my_browser["pwd"] = "ThunderDude"
+my_response = my_browser.submit()  # submit form
 
 # Make sure we were redirected to the profiles page
-print myResponse.geturl()
+print my_response.geturl()
 
 # Return to the previous login page
-myBrowser.back()
+my_browser.back()
 
 # Try to log in with incorrect information
-myBrowser.select_form("login")
-myBrowser["user"] = "Hera"
-myBrowser["pwd"] = "LightningLady"
-myResponse = myBrowser.submit()  # submit form
+my_browser.select_form("login")
+my_browser["user"] = "Hera"
+my_browser["pwd"] = "LightningLady"
+my_response = my_browser.submit()  # submit form
 
 # See if we were redirected to the "error" page by searching for known text
-if myResponse.get_data().find("Wrong username or password!") != -1:
+if my_response.get_data().find("Wrong username or password!") != -1:
     print "Login failed."
 else:
     print "Login success."
