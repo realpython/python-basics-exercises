823553fe6be70098b809c28ff952a0f2852b3b1f
fheisler <fheisler@gmail.com>
2014-07-19 22:30:04 -0400

    Var name updates

--- Course materials - Python 3/Chapter 13/Assignment solutions/13-1 review exercises.py
+++ Course materials - Python 3/Chapter 13/Assignment solutions/13-1 review exercises.py
@@ -10,12 +10,12 @@ with sqlite3.connect(':memory:') as connection:
     c.execute("CREATE TABLE Roster(Name TEXT, Species TEXT, IQ INT)")
 
     # Add some data into the database
-    rosterData = (
+    roster_data = (
         ("Jean-Baptiste Zorg", "Human", 122),
         ("Korben Dallas", "Meat Popsicle", 100),
         ("Ak'not", "Mangalore", -5)
     )
-    c.executemany("INSERT INTO Roster VALUES(?, ?, ?)", rosterData)
+    c.executemany("INSERT INTO Roster VALUES(?, ?, ?)", roster_data)
 
     # Update the Species of Korben Dallas to "Human"
     c.execute("UPDATE Roster SET Species=? WHERE Name=?",
