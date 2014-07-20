abe92dca7e3bc2553cf6b32e94fa5610149f675f
fheisler <fheisler@gmail.com>
2014-07-19 22:22:48 -0400

    Var name updates

--- Course materials - Python 3/Chapter 10/Assignment solutions/10-1 review exercises.py
+++ Course materials - Python 3/Chapter 10/Assignment solutions/10-1 review exercises.py
@@ -8,37 +8,37 @@ be placed in the Chapter 10 "Practice files" folder.
 '''
 
 # Read a text file by looping over individual lines
-myPoem = open("poem.txt", "r")
-for line in myPoem.readlines():
+my_poem = open("poem.txt", "r")
+for line in my_poem.readlines():
     # Replace automatic line break at end of line;
     # file already contains newline characters
     print(line,)
-myPoem.close()
+my_poem.close()
 
 # Print some blank lines to separate the two examples
 print("\n\n")
 
 # Use "with" to automatically close a file when finished
-with open("poem.txt", "r") as myPoem:
-    for line in myPoem.readlines():
+with open("poem.txt", "r") as my_poem:
+    for line in my_poem.readlines():
         print(line,)
 
 # Write the contents of one file into another, line-by-line
-poemIn = open("poem.txt", "r")
-poemOut = open("output.txt", "w")
-for line in poemIn.readlines():
-    poemOut.write(line)
-poemIn.close()
-poemOut.close()
+poem_in = open("poem.txt", "r")
+poem_out = open("output.txt", "w")
+for line in poem_in.readlines():
+    poem_out.write(line)
+poem_in.close()
+poem_out.close()
 
 # Repeat the previous exercise using the "with" keyword
 # (This will overwrite the previous output file.)
-with open("poem.txt", "r") as poemIn, open("output.txt", "w") as poemOut:
-    for line in poemIn.readlines():
-        poemOut.write(line)
+with open("poem.txt", "r") as poem_in, open("output.txt", "w") as poem_out:
+    for line in poem_in.readlines():
+        poem_out.write(line)
 
 
 # Append a new line to the end of "output.txt"
 # (Need to start on a new line, so add "\n" to the beginning.)
-with open("output.txt", "a") as poemAppend:
-    poemAppend.write("\nThus ends the haiku.")
+with open("output.txt", "a") as poem_append:
+    poem_append.write("\nThus ends the haiku.")
