238e0c7096d02a09f0abeabd47112ffee2f024c3
fheisler <fheisler@gmail.com>
2014-07-19 20:56:19 -0400

    Var name updates

--- Course materials/Chapter 8/Assignment solutions/8-6 review exercises.py
+++ Course materials/Chapter 8/Assignment solutions/8-6 review exercises.py
@@ -13,5 +13,6 @@ trials = 10000
 total = 0
 for trial in range(0, trials):
     total += randint(1, 6)
-avgResult = total / trials
-print "The average result of {} throws was {}".format(trials, avgResult)
+
+avg_result = total / trials
+print "The average result of {} throws was {}".format(trials, avg_result)
