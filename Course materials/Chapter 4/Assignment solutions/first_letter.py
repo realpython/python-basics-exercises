c0fa61d1bb922edef56b5e80b498eb38c113855e
fheisler <fheisler@gmail.com>
2014-07-19 20:50:37 -0400

    Var name updates

--- Course materials/Chapter 4/Assignment solutions/first_letter.py
+++ Course materials/Chapter 4/Assignment solutions/first_letter.py
@@ -1,6 +1,6 @@
 # 4.3 first_letter.py
 # Return the upper-case first letter entered by the user
 
-userInput = raw_input("Tell me your password: ")
-firstLetter = userInput[0]
-print "The first letter you entered was:", firstLetter.upper()
+user_input = raw_input("Tell me your password: ")
+first_letter = user_input[0]
+print "The first letter you entered was:", first_letter.upper()
