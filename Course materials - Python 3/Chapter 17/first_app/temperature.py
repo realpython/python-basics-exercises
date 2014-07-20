dd3290960204a76def239989505fe7aae2494b2d
fheisler <fheisler@gmail.com>
2014-07-19 22:41:31 -0400

    Var name updates

--- Course materials - Python 3/Chapter 17/first_app/temperature.py
+++ Course materials - Python 3/Chapter 17/first_app/temperature.py
@@ -1,22 +1,22 @@
 import webapp2
 
 
-def convertTemp(celTemp):
+def convert_temp(cel_temp):
     ''' convert Celsius temperature to Fahrenheit temperature '''
-    if celTemp == "":
+    if cel_temp == "":
         return ""
     try:
-        farTemp = float(celTemp) * 9/5 + 32
-        farTemp = round(farTemp, 3)  # round to three decimal places
-        return str(farTemp)
+        far_temp = float(cel_temp) * 9/5 + 32
+        far_temp = round(far_temp, 3)  # round to three decimal places
+        return str(far_temp)
     except ValueError:  # user entered non-numeric temperature
         return "invalid input"
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        celTemp = self.request.get("celTemp")
-        farTemp = convertTemp(celTemp)
+        cel_temp = self.request.get("cel_temp")
+        far_temp = convert_temp(cel_temp)
 
         self.response.headers["Content-Type"] = "text/html"
         self.response.write("""
@@ -25,12 +25,12 @@ class MainPage(webapp2.RequestHandler):
             <body>
               <form action="/" method="get">
                 Celsius temperature: <input type="text"
-                                        name="celTemp" value={}>
+                                        name="cel_temp" value={}>
                 <input type="submit" value="Convert"><br>
                 Fahrenheit temperature: {}
               </form>
             </body>
-          </html>""".format(celTemp, farTemp))
+          </html>""".format(cel_temp, far_temp))
 
 routes = [('/', MainPage)]
-myApp = webapp2.WSGIApplication(routes, debug=True)
+my_app = webapp2.WSGIApplication(routes, debug=True)
