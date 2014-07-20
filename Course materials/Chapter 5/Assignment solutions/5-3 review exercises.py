02042210650aef1f1d5548c01f2af8f7e99c7210
fheisler <fheisler@gmail.com>
2014-07-19 20:52:02 -0400

    Var name updates

--- Course materials/Chapter 5/Assignment solutions/5-3 review exercises.py
+++ Course materials/Chapter 5/Assignment solutions/5-3 review exercises.py
@@ -8,10 +8,10 @@ print "AAA".find("a")
 # Try to find a number inside a string;
 # use str() to convert the number first
 version = "version 2.0"
-vNum = 2.0
-print version.find(str(vNum))
+v_num = 2.0
+print version.find(str(v_num))
 
 
 # Try to find an upper-case "X" in user input:
-myInput = raw_input("Type something: ")
-print myInput.find("X")
+my_input = raw_input("Type something: ")
+print my_input.find("X")
