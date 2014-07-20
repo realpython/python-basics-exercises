7a07d9d2e0d08e11f5fed3b2d50f402ab6416936
fheisler <fheisler@gmail.com>
2014-07-19 21:07:17 -0400

    Var name updates

--- Course materials/Chapter 12/Assignment solutions/cover_the_emperor.py
+++ Course materials/Chapter 12/Assignment solutions/cover_the_emperor.py
@@ -5,24 +5,24 @@ import os
 from pyPdf import PdfFileReader, PdfFileWriter
 
 path = "C:/Real Python/Course materials/Chapter 12/Practice files"
-inputFileName1 = os.path.join(path, "Emperor cover sheet.pdf")
-inputFile1 = PdfFileReader(file(inputFileName1, "rb"))
-inputFileName2 = os.path.join(path, "The Emperor.pdf")
-inputFile2 = PdfFileReader(file(inputFileName2, "rb"))
-outputPDF = PdfFileWriter()
+input_file_name1 = os.path.join(path, "Emperor cover sheet.pdf")
+input_file1 = PdfFileReader(file(input_file_name1, "rb"))
+input_file_name2 = os.path.join(path, "The Emperor.pdf")
+input_file2 = PdfFileReader(file(input_file_name2, "rb"))
+output_PDF = PdfFileWriter()
 
 # Read in all pages from the cover sheet PDF file
-for pageNum in range(0, inputFile1.getNumPages()):
-    page = inputFile1.getPage(pageNum)
-    outputPDF.addPage(page)
+for page_num in range(0, input_file1.getNumPages()):
+    page = input_file1.getPage(page_num)
+    output_PDF.addPage(page)
 
 # Read in all pages from "The Emperor.pdf" into the same output file
-for pageNum in range(0, inputFile2.getNumPages()):
-    page = inputFile2.getPage(pageNum)
-    outputPDF.addPage(page)
+for page_num in range(0, input_file2.getNumPages()):
+    page = input_file2.getPage(page_num)
+    output_PDF.addPage(page)
 
 # Output the results into a new PDF
-outputFileName = os.path.join(path, "Output/The Covered Emperor.pdf")
-outputFile = file(outputFileName, "wb")
-outputPDF.write(outputFile)
-outputFile.close()
+output_file_name = os.path.join(path, "Output/The Covered Emperor.pdf")
+output_file = file(output_file_name, "wb")
+output_PDF.write(output_file)
+output_file.close()
