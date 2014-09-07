71fc8d569241b37dd0c89f6eb80e5c888454a86c
Michael Herman <hermanmu@gmail.com>
2014-09-06 21:17:20 -0500

    added solution to cats with hats

---
+++ Course materials - Python 3/Chapter 9/Assignment solutions/cats_with_hats.py
@@ -0,0 +1,16 @@
+def get_cats_with_hats(array_of_cats):
+    cats_with_hats_on = []
+    for num in range(1, 100+1):
+        for cat in range(1, 100):
+            if cat % num == 0:
+                if array_of_cats[cat] is True:
+                    array_of_cats[cat] = False
+                else:
+                    array_of_cats[cat] = True
+    for cat in range(1, 100):
+        if cats[cat] is True:
+            cats_with_hats_on.append(cat)
+    return cats_with_hats_on
+
+cats = [False] * 100
+print(get_cats_with_hats(cats))
