5fa61b3248452f8ce1b5e12425ac4937e17682d5
Michael Herman <hermanmu@gmail.com>
2015-12-22 09:24:55 -0700

    updated chp6 solutions for pep8

--- refactor/chp06/solutions/temperature.py
+++ refactor/chp06/solutions/temperature.py
@@ -3,12 +3,12 @@
 
 
 def convert_cel_to_far(cel_temp):
-    far_temp = cel_temp * 9/5 + 32
+    far_temp = cel_temp * 9 / 5 + 32
     return far_temp
 
 
 def convert_far_to_cel(far_temp):
-    cel_temp = (far_temp - 32) * 5/9
+    cel_temp = (far_temp - 32) * 5 / 9
     return cel_temp
 
 temp1 = 72
