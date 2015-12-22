166672e0196bbe71c630a625e99058991b200d79
topleft <pete.james.jeffryes@gmail.com>
2015-12-21 17:19:07 -0700

    reviewed and added chap 5 exercises

---
+++ refactor/Chapter 5/Assignment solutions/5-3 review exercises.py
@@ -0,0 +1,17 @@
+# 5.3 review exercises
+
+
+# Cannot find the string "a" in the string "AAA":
+print("AAA".find("a"))
+
+
+# Try to find a number inside a string;
+# use str() to convert the number first
+version = "version 2.0"
+v_num = 2.0
+print(version.find(str(v_num)))
+
+
+# Try to find an upper-case "X" in user input:
+my_input = input("Type something: ")
+print(my_input.find("X"))
