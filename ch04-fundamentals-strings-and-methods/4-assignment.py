8e8f3afd3a31ee262eb13758448b6a6d2584158e
David Amos <somacdivad@gmail.com>
2018-10-07 13:29:04 -0500

    Remove 'Fundamentals' from chapter names

--- ch04-fundamentals-strings-and-methods/4-assignment.py
+++
@@ -1,9 +0,0 @@
-# 4.4 - Assignment: Pick Apart Your User's Input
-# Solution to assignment
-
-
-# Return the upper-case first letter entered by the user
-
-user_input = input("Tell me your password: ")
-first_letter = user_input[0]
-print("The first letter you entered was:", first_letter.upper())
