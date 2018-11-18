1ec2aeb6de10576430864e0a2b22f2e4269a631a
David Amos <somacdivad@gmail.com>
2018-11-18 08:56:51 -0600

    More renaming and added solution to new exercise in 4.2

--- ch04-strings-and-methods/2-basic-string-operations.py
+++ ch04-strings-and-methods/2-basic-string-operations.py
@@ -20,6 +20,13 @@ string_two = "jeebies"
 print(string_one, string_two)
 
 
+# Exercise 3
+# Display two strings together, with a space in between
+string_one = "heebie"
+string_two = "jeebies"
+print(string_one + " " + string_two)
+
+
 # Exercise 4
 # Use subscripting to display part of a string
 print("bazinga"[2:6])
