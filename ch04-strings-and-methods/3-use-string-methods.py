1a16a5d2e05224332b81cf6ca5a39ee685b55be2
David Amos <somacdivad@gmail.com>
2018-11-20 21:14:22 -0600

    PEP8 pass

--- ch04-strings-and-methods/3-use-string-methods.py
+++ ch04-strings-and-methods/3-use-string-methods.py
@@ -37,16 +37,16 @@ string2 = "becomes"
 string3 = "BEAR"
 string4 = "  bEautiful"
 
-print (string1.startswidth("re"))
-print (string2.startswidth("re"))
-print (string3.startswidth("re"))
-print (string4.startswidth("re"))
+print(string1.startswidth("re"))
+print(string2.startswidth("re"))
+print(string3.startswidth("re"))
+print(string4.startswidth("re"))
 
 string1 = string1.lower()
 string3 = string2.lower()
 string4 = string4.strip().lower()
 
-print (string1.startswidth("re"))
-print (string2.startswidth("re"))
-print (string3.startswidth("re"))
-print (string4.startswidth("re"))
+print(string1.startswidth("re"))
+print(string2.startswidth("re"))
+print(string3.startswidth("re"))
+print(string4.startswidth("re"))
