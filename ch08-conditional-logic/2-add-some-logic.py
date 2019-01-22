3a46af9034f9cc551f4140b0823010c1adaff030
David Amos <somacdivad@gmail.com>
2019-01-21 20:35:51 -0600

    Add solutions to exercise 8.2.2

--- ch08-conditional-logic/2-add-some-logic.py
+++ ch08-conditional-logic/2-add-some-logic.py
@@ -1,10 +1,22 @@
 # 8.2 - Add Some Logic
 # Solutions to review exercises
 
-
+# --- Exercise 1
 # Test whether these expressions are True or False
-
 print((1 <= 1) and (1 != 1))
 print(not (1 != 2))
 print(("good" != "bad") or False)
 print(("good" != "Good") and not (1 == 1))
+
+# --- Exercise 2
+# Add parentheses so that the following expressions all
+# evaluate to True
+
+# False == not True
+print(False == (not True))
+# True and False == True and False
+print((True and False) == (True and False))
+# not True and "A" == "B"
+print(not (True and "A" == "B"))
+# "B" and not "A" != "B"
+print(("B" and not "A") != "B")
