c160b445e2c8a92a53b86f49f50b4fa273afdbaf
David Amos <somacdivad@gmail.com>
2019-03-19 16:17:50 -0500

    Add hints to coin toss challenge solutions

--- ch08-conditional-logic/9c-challenge.py
+++ ch08-conditional-logic/9c-challenge.py
@@ -4,6 +4,12 @@
 
 # Simulate the results of a series of coin tosses and track the results
 
+# This one is tricky to structure correctly. Try writing out the logic before you start coding. Some additional pointers if you're stuck:
+# 1. You will need to use a `for` loop over a range of trials.
+# 2. For each trial, first you should check the outcome of the first flip.
+# 3. Make sure you add the first flip to the total number of flips.
+# 4. After the first toss, you'll need another loop to keep flipping while you get the same result as the first flip.
+
 from random import randint
 
 
