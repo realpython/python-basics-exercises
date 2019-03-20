197627e3b422bf2b689c836fa8965ec2cfb2cfd8
David Amos <somacdivad@gmail.com>
2019-03-20 09:42:46 -0500

    Update solutions to 8.7

--- ch08-conditional-logic/7-simulate-events-and-calculate-probabilities.py
+++ ch08-conditional-logic/7-simulate-events-and-calculate-probabilities.py
@@ -6,15 +6,20 @@ from random import randint
 
 
 # Exercise 1
-# Simulate the toss of a die
-print(randint(1, 6))
+# Write a function that simulatee the roll of a die.
+def roll():
+    """Return random integer between 1 and 6"""
+    return randint(1, 6)
 
 
 # Exercise 2
-# Simulate 10,000 throws of dice and display the average result
+# Simulate 10,000 rolls of a die and display the average number rolled.
 trials = 10000
 total = 0
+
 for trial in range(0, trials):
-    total += randint(1, 6)
-avg_result = total / trials
-print(f"The average result of {trials} throws was {avg_result}")
+    total = total + roll()
+
+avg_roll = total / trials
+
+print(f"The average result of {trials} throws is {avg_roll}")
