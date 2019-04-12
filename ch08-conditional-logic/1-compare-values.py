bd06bff2b21edc78ee6489601231e1fc367a733a
David Amos <somacdivad@gmail.com>
2019-04-12 12:51:09 -0500

    New exercise

--- ch08-conditional-logic/1-compare-values.py
+++ ch08-conditional-logic/1-compare-values.py
@@ -2,6 +2,7 @@
 # Solutions to review exercises
 
 
+# Exercise 1
 # Test whether these expressions are True or False
 
 print(1 <= 1)
@@ -10,3 +11,28 @@ print(1 != 2)
 print("good" != "bad")
 print("good" != "Good")
 print(123 == "123")
+
+
+# Exercise 2
+# Fill in the blank so that each of the following expressions are True
+
+# 3 __ 4
+# Any of the following:
+3 < 4
+3 <= 4
+3 != 4
+
+# 10 __ 5
+# Any of the following:
+10 > 5
+10 >= 5
+10 != 5
+
+# "jack" __ "jill"
+# Any of the following:
+"jack" < "jill"
+"jack" <= "jill"
+"jack" != "jill"
+
+# 42 __ "42"
+42 != "42"
