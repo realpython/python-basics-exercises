035756fd4f8ebccd7e8388fa32a0957fc821612c
David Amos <somacdivad@gmail.com>
2019-06-19 13:47:55 -0500

    Add solution to new exercise

--- ch08-conditional-logic/6-recover-from-errors.py
+++ ch08-conditional-logic/6-recover-from-errors.py
@@ -2,13 +2,27 @@
 # Solution to review exercises
 
 
+# Exercise 1
 # Ask the user to enter an integer.
 # Repeat the process if the user hasn't entered an integer.
-repeat = True
-while repeat:
+while True:
     try:
         my_input = input("Type an integer: ")
         print(int(my_input))
-        repeat = False
+        break
     except ValueError:
         print("try again")
+
+
+# Exercise 2
+# Print character and specifid index in string
+
+input_string = input("Enter a string: ")
+
+try:
+    index = int(input("Enter an integer: "))
+    print(input_string[index])
+except ValueError:
+    print("Invalid number")
+except IndexError:
+    print("Index is out of bounds")
