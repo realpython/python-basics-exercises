e5fb321db6983ba33dc6dc2e76f87f64a9d6e9de
David Amos <somacdivad@gmail.com>
2019-07-03 11:02:32 -0500

    Fix missing space in string

--- ch05-numbers-in-python/5-math-functions-and-number-methods.py
+++ ch05-numbers-in-python/5-math-functions-and-number-methods.py
@@ -16,6 +16,6 @@ print(f"The absolute value of {num} is {abs(num)}")
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
 print(
-    f"The difference between {num1} and {num2} is an integer?"
+    f"The difference between {num1} and {num2} is an integer? "
     f"{(num1 - num2).is_integer()}!"
 )
