1f155a3f280497b5ff89567e4daa07d4fc92188d
David Amos <somacdivad@gmail.com>
2019-09-05 16:48:59 -0500

    Clean-up capital city loop challenge

--- ch09-lists-tuples-and-dictionaries/7-challenge-capital-city-loop.py
+++ ch09-lists-tuples-and-dictionaries/7-challenge-capital-city-loop.py
@@ -1,23 +1,69 @@
 # 9.7 - Challenge: Capital City Loop
 # Solution to challenge
 
-
-from capitals import capitals_dict
 import random
 
+capitals_dict = {
+    "Alabama": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "Wyoming": "Cheyenne",
+}
 
-def capital_game(state, capital):
-    while True:
-        guess = input(f"What is the capital of '{state}'? ").lower()
-        if guess == "exit":
-            print(f"The capital of '{state}' is '{capital}'.")
-            print("Goodbye")
-            break
-        elif guess == (capital).lower():
-            print("Correct! Nice job.")
-            break
+state, capital = random.choice(list(capitals_dict.items()))
 
-
-state = random.choice(list(capitals_dict.keys()))
-capital = capitals_dict[state]
-capital_game(state, capital)
+while True:
+    guess = input(f"What is the capital of '{state}'? ").lower()
+    if guess == "exit":
+        print(f"The capital of '{state}' is '{capital}'.")
+        print("Goodbye")
+        break
+    elif guess == capital.lower():
+        print("Correct! Nice job.")
+        break
