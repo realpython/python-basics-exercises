01c0a1240f85aafaa0e7e220d069ab6867481d95
David Amos <somacdivad@gmail.com>
2019-09-10 01:44:43 -0500

    Better naming in solution

--- ch08-conditional-logic/5-break-out-of-the-pattern.py
+++ ch08-conditional-logic/5-break-out-of-the-pattern.py
@@ -5,8 +5,8 @@
 # Exercise 1
 # Run in an infinite loop until the user types "q" or "Q"
 while True:
-    my_input = input('Type "q" or "Q" to quit: ')
-    if my_input.upper() == "Q":
+    user_input = input('Type "q" or "Q" to quit: ')
+    if user_input.upper() == "Q":
         break
 
 
