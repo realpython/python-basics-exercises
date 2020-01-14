1bac150648227fd119fac5912c306e7f7ec67905
David Amos <somacdivad@gmail.com>
2020-01-14 13:43:34 -0600

    Add ch 13 second challenge practice file and solution

---
+++ ch13-interact-with-pdf-files/7-challenge-unscramble-a-pdf.py
@@ -0,0 +1,32 @@
+# 13.7 - Challenge: Unscramble a PDF
+# Solution to challenge
+
+from pathlib import Path
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+def get_page_text(page):
+    return page.extractText()
+
+
+pdf_path = Path.home() / "github/realpython/python-basics-exercises/" \
+    "ch13-interact-with-pdf-files/practice_files/scrambled.pdf"
+
+pdf_reader = PdfFileReader(str(pdf_path))
+pdf_writer = PdfFileWriter()
+
+pages = list(pdf_reader.pages)
+pages.sort(key=get_page_text)
+
+for page in pages:
+    rotation_degrees = page["/Rotate"]
+    if rotation_degrees < 0:
+        page.rotateClockwise(-rotation_degrees)
+    elif rotation_degrees > 0:
+        page.rotateCounterClockwise(rotation_degrees)
+    pdf_writer.addPage(page)
+
+output_path = Path.home() / "unscrambled.pdf"
+with output_path.open(mode="wb") as output_file:
+    pdf_writer.write(output_file)
