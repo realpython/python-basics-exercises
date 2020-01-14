414fc48cb17995150334bb9b2b30a7cd762f271c
David Amos <somacdivad@gmail.com>
2020-01-13 21:12:40 -0600

    Add solution to ch13 section 3 exercise 2

--- ch13-interact-with-pdf-files/3-concatenating-and-merging-pdfs.py
+++ ch13-interact-with-pdf-files/3-concatenating-and-merging-pdfs.py
@@ -27,3 +27,26 @@ for path in pdf_paths:
 output_path = Path.home() / "concatenated.pdf"
 with output_path.open(mode="wb") as output_file:
     pdf_merger.write(output_file)
+
+
+# ***********
+# Exercise 2
+#
+# Using the same `PdfFileMerger` instance from exercise 1, merge the
+# file `merge3.pdf` in-between the pages from `merge1.pdf` and
+# `merge2.pdf` using `.merge()`.
+#
+# The final result should be a PDF with three pages. The first page
+# should have the number `1` on it, the second should have `2`, and the
+# third should have `3`.
+# ***********
+
+pdf_merger = PdfFileMerger()
+pdf_merger.append(str(output_path))
+
+pdf_path = BASE_PATH / "merge3.pdf"
+pdf_merger.merge(1, str(pdf_path))
+
+output_path = Path.home() / "merged.pdf"
+with output_path.open(mode="wb") as output_file:
+    pdf_merger.write(output_file)
