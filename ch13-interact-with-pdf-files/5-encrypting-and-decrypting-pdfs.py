504181592820ce42035009b664557e9fa985262c
David Amos <somacdivad@gmail.com>
2020-01-13 22:49:03 -0600

    Add ch13 section 5 exercise 2 solution

--- ch13-interact-with-pdf-files/5-encrypting-and-decrypting-pdfs.py
+++ ch13-interact-with-pdf-files/5-encrypting-and-decrypting-pdfs.py
@@ -27,3 +27,19 @@ pdf_writer.encrypt(user_pwd="Unguessable")
 output_path = Path.home() / "top_secret_encrypted.pdf"
 with output_path.open(mode="wb") as output_file:
     pdf_writer.write(output_file)
+
+
+# ***********
+# Exercise 2
+#
+# Open the `top_secret_encrpyted.pdf` file you created in exercise 1,
+# decrypt it, and print the text on the first page of the PDF.
+# ***********
+
+pdf_path = Path.home() / "top_secret_encrypted.pdf"
+pdf_reader = PdfFileReader(str(pdf_path))
+
+pdf_reader.decrypt("Unguessable")
+
+first_page = pdf_reader.getPage(0)
+print(first_page.extractText())
