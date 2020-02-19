97bc2857d939fe0d49bf0a70b3adc36806d5da2c
David Amos <somacdivad@gmail.com>
2020-02-19 06:26:03 -0600

    Remove extra coin flip and clean up challenge solution

--- ch08-conditional-logic/8c-challenge-simulate-a-coin-toss-experiment.py
+++ ch08-conditional-logic/8c-challenge-simulate-a-coin-toss-experiment.py
@@ -12,26 +12,38 @@
 # 4. After the first toss, you'll need another loop to keep flipping while you
 #    get the same result as the first flip.
 
-from random import randint
+import random
 
 
 def single_trial():
-    toss = randint(0, 1)
-    total_flips = 1
+    """Simulate repeatedly a coing  until both heads and tails are seen."""
+    # This function uses random.randint() to simulate a single coin toss.
+    # randing(0, 1) randomly returns 0 or 1 with equal probability. We can
+    # use 0 to represent heads and 1 to represent tails.
 
-    while toss == randint(0, 1):
-        total_flips += 1
-        toss = randint(0, 1)
+    # Flip the coin the first time
+    flip_result = random.randint(0, 1)
+    # Keep a tally of how many times the coin has been flipped. We've only
+    # flipped once so the initial count is 1.
+    flip_count = 1
 
-    total_flips += 1
-    return total_flips
+    # Continue to flip the coin until randint(0, 1) returns something
+    # different than the original flip_result
+    while flip_result == random.randint(0, 1):
+        flip_count = flip_count + 1
+
+    # The last step in the loop flipped the coin but didn't update the tally,
+    # so we need to increase the flip_count by 1
+    flip_count = flip_count + 1
+    return flip_count
 
 
 def flip_trial_avg(num_trials):
+    """Calculate the average number of flips per trial over num_trials total trials."""
     total = 0
     for trial in range(num_trials):
-        total += single_trial()
+        total = total + single_trial()
     return total / num_trials
 
 
-print(f"The average number of coin flips was {flip_trial_avg(10000)}")
+print(f"The average number of coin flips was {flip_trial_avg(10_000)}")
