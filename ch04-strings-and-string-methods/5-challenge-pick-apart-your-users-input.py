3a5bc4254188d1bfb5e15556beacb266897be33d
David Amos <somacdivad@gmail.com>
2020-04-29 16:23:02 -0500

    Use string concatenation

--- ch04-strings-and-string-methods/5-challenge-pick-apart-your-users-input.py
+++ ch04-strings-and-string-methods/5-challenge-pick-apart-your-users-input.py
@@ -6,4 +6,4 @@
 
 user_input = input("Tell me your password: ")
 first_letter = user_input[0]
-print("The first letter you entered was:", first_letter.upper())
+print("The first letter you entered was: " + first_letter.upper())
