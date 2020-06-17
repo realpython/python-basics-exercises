1f411c570852d3ff652c364d52ed74c9f65c4a6c
lsdores <67026999+lsdores@users.noreply.github.com>
2020-06-17 10:37:29 +0200

    Simpler solution to coin toss challenge 8a
    I wanted to compare my solution with the one on the repository. It turns out that my solution was a bit simpler with no need of the conditionals it might also be a bit easier to read, so I decided to suggest it.

--- ch08-conditional-logic/8a-challenge-simulate-a-coin-toss-experiment.py
+++ ch08-conditional-logic/8a-challenge-simulate-a-coin-toss-experiment.py
@@ -27,29 +27,19 @@ flips = 0
 num_trials = 10_000
 
 for trial in range(num_trials):
-    if coin_flip() == "heads":
-        # Increment the number of flips by 1
+    # The first flip can be either "heads" or "tails"
+    # Increment the number of flips by 1 after first flip
+    first_flip = coin_flip()
+    flips = flips + 1
+    while coin_flip() == first_flip:
+        # Keep incrementing the total number of flips
+        # until the value returned by coin_flip()
+        # is different from first_flip
         flips = flips + 1
-        while coin_flip() == "heads":
-            # Keep incrementing the total number of flips
-            # until "tails" is returned by coin_flip()
-            flips = flips + 1
-        # Once coin_flip() return "tails", the loop will exit,
-        # but we need to add one more to flips to track the
-        # last flip that generated "tails"
-        flips = flips + 1
-    else:
-        # coin_flip() returned "tails" on the first flip.
-        # Increment the number of flips by 1
-        flips = flips + 1
-        while coin_flip() == "tails":
-            # Keep incrementing the total number of flips
-            # until "heads" is returned by coin_flip()
-            flips = flips + 1
-        # Once coin_flip() returns "heads", the loop will exit,
-        # but we need to add one more to flips to track the
-        # last flip that generated "heads"
-        flips = flips + 1
-
+    # Once coin_flip() is different from first_flip, 
+    # the loop will exit, but we need to add one more 
+    # to flips to track the last flip
+    flips = flips + 1
+    
 avg_flips_per_trial = flips / num_trials
 print(f"The average number of flips per trial is {avg_flips_per_trial}.")
