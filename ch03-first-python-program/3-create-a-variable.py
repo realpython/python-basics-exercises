49bcfb3105a65cf9fdd47bd0739c71b7f02bb320
Yash Umesh Pukale <70129255+Yash-Pukale@users.noreply.github.com>
2020-09-30 19:37:17 +0530

    Added an example of typecasting

--- ch03-first-python-program/3-create-a-variable.py
+++ ch03-first-python-program/3-create-a-variable.py
@@ -13,3 +13,13 @@ print("hello")
 # Display the contents of a string variable
 my_string = "hi"
 print(my_string)
+
+
+# Declare variables
+sample_string = "Hello"
+sample_int = 123
+
+#To concat and display both the above variables, the integer variable needs to be typecasted !!
+print(sample_string + str(sample_int))
+# Can also be written using F-string
+print(f"{sample_string + str(sample_int)}")
