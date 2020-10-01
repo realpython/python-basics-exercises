e21d85c8d497bbd0399cb3c91a28c81d91b421ad
Kushagra <63976266+CodeGodZeus@users.noreply.github.com>
2020-10-01 11:59:14 +0530

    Update 3-challenge-convert-temperatures.py

--- ch06-functions-and-loops/3-challenge-convert-temperatures.py
+++ ch06-functions-and-loops/3-challenge-convert-temperatures.py
@@ -4,7 +4,7 @@
 
 def convert_cel_to_far(temp_cel):
     """Return the Celsius temperature temp_cel converted to Fahrenheit."""
-    temp_far = temp_cel * (9 / 5) + 32
+    temp_far = temp_cel * (9 / 5) + 32       
     return temp_far
 
 
@@ -32,7 +32,7 @@ print(f"{temp_far} degrees F = {temp_cel:.2f} degrees C")
 temp_cel = input("\nEnter a temperature in degrees C: ")
 
 # Convert the temperature to Fahrenheit.
-temp_far = convert_cel_to_far(float(temp_cel))
+temp_far = convert_cel_to_far(float(temp_cel))          #using float datatype to change temperature from celsius to fahrenheit
 
 # Display the converted temperature
 print(f"{temp_cel} degrees C = {temp_far:.2f} degrees F")
