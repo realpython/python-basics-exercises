7f969289b676a7182a80c9c3144a4f79927cda72
Kushagra <63976266+CodeGodZeus@users.noreply.github.com>
2020-10-01 11:57:19 +0530

    Update 1-integers-and-floating-point-numbers.py

--- ch05-numbers-in-python/1-integers-and-floating-point-numbers.py
+++ ch05-numbers-in-python/1-integers-and-floating-point-numbers.py
@@ -5,11 +5,11 @@
 # Exercise 1
 num1 = 25_000_000
 num2 = 25000000
-print(num1)
-print(num2)
+print(num1)   #produce output of num1
+print(num2)    #produce output of num2
 
 # Exercise 2
-num = 1.75e5
+num = 1.75e5  #produce output dof num
 print(num)
 
 # Exercise 3
