7230674f743625cf87caf1ec3c5797c1c759ed3b
lokesh108 <46420483+lokesh108@users.noreply.github.com>
2020-10-25 22:18:50 +0530

    Update 2-screw-things-up.py

--- ch03-first-python-program/2-screw-things-up.py
+++ ch03-first-python-program/2-screw-things-up.py
@@ -4,11 +4,11 @@
 
 # Exercise 1
 # The following line won't run because of a syntax error
-print("hi)
+print('hi)
 
 # We didn't close the double quotes at the end of the string.
 # The line above needed to have been:
-# print("hi")
+# print('hi')
 
 
 # Exercise 2
@@ -22,5 +22,5 @@ print(hello)
 #
 # This line could have been:
 #
-# my_string = "hello"
-# print(my_string)
+ my_string = 'hello'
+ print(my_string)
