026379d0cdd68df5c80f71f921e531ecdc6557e6
Dan Bader <mail@dbader.org>
2022-06-13 10:28:44 -0700

    Add number guessing exercise solution

--- ch08-conditional-logic/3-control-the-flow-of-your-program.py
+++ ch08-conditional-logic/3-control-the-flow-of-your-program.py
@@ -2,6 +2,7 @@
 # Solutions to review exercises
 
 
+# --- Exercise 1
 # Display whether the length of user input is <, > or = 5 characters
 
 my_input = input("Type something: ")
@@ -12,3 +13,15 @@ elif len(my_input) > 5:
     print("Your input is greater than 5 characters long.")
 else:
     print("Your input is 5 characters long.")
+
+
+# --- Exercise 2
+# Number guessing program ("guess" the number 3)
+
+print("I'm thinking of a number between 1 and 10. Guess which one.")
+my_guess = input("Type in your guess: ")
+
+if my_guess == "3":
+    print("You win!")
+else:
+    print("You lose.")
