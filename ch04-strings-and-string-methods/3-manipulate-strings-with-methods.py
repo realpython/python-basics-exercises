c4edec716060fe93a465c3955f61628ae716308e
Martin Breuss <martin-martin@users.noreply.github.com>
2023-10-15 13:37:50 +0200

    Fix technical typo

--- ch04-strings-and-string-methods/3-manipulate-strings-with-methods.py
+++ ch04-strings-and-string-methods/3-manipulate-strings-with-methods.py
@@ -27,7 +27,7 @@ string2 = "Brisket    "
 string3 = "  Cheeseburger   "
 
 print(string1.strip())  # Could also use .lstrip()
-print(string1.strip())  # Could also use .rstrip()
+print(string2.strip())  # Could also use .rstrip()
 print(string3.strip())
 
 
